7f3253c15295675e2b4982b8130fd9dae6cb08bc
Péter Molnár <xodax9898@gmail.com>
2022-05-22 16:45:11 +0200

    errror_sql export

--- database/error_tracker.sql
+++ database/error_tracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1:3306
--- Létrehozás ideje: 2022. Ápr 24. 18:51
+-- Létrehozás ideje: 2022. Máj 22. 14:44
 -- Kiszolgáló verziója: 5.7.36
 -- PHP verzió: 7.4.26
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `wp2`
+-- Adatbázis: `error_tracker`
 --
 
 -- --------------------------------------------------------
@@ -30,13 +30,24 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `errors`;
 CREATE TABLE IF NOT EXISTS `errors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `error_id` int(11) NOT NULL AUTO_INCREMENT,
   `error_name` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
   `error_level` int(10) NOT NULL,
   `error_type` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
-  `has_solution` bit(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+  `has_solution` varchar(10) COLLATE utf8_hungarian_ci NOT NULL,
+  PRIMARY KEY (`error_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `errors`
+--
+
+INSERT INTO `errors` (`error_id`, `error_name`, `error_level`, `error_type`, `has_solution`) VALUES
+(27, 'asdasdasfsdg', 10, 'Logistical', 'No'),
+(26, 'ewqrqfvasc', 2, 'Logistical', 'No'),
+(24, 'sads', 2, 'Logistical', 'No'),
+(25, 'asdasgad', 2, 'Logistical', 'No'),
+(15, 'camogration of ionized flexibility', 2, 'Logistical', 'No');
 
 -- --------------------------------------------------------
 
@@ -100,8 +111,19 @@ CREATE TABLE IF NOT EXISTS `users` (
   `first_name` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
   `last_name` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
   `role` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
+  `email` varchar(250) COLLATE utf8_hungarian_ci NOT NULL,
+  `img_path` varchar(250) COLLATE utf8_hungarian_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `token`, `first_name`, `last_name`, `role`, `email`, `img_path`) VALUES
+(1, 'Pylnpt', 'cbfdac6008f9cab4083784cbd1874f76618d2a97', 'Molnár', 'Péter', 'shift_leader', 'molnar.peter9819@gmail.com', 'images/main_plan.png'),
+(2, 'alfi', 'cbfdac6008f9cab4083784cbd1874f76618d2a97', 'Kiss', 'Alfonz', 'mechanic', 'kiss@gmail.com', 'images/myprofile.jpg'),
+(3, 'Dani', 'cbfdac6008f9cab4083784cbd1874f76618d2a97', 'Kiss', 'Dániel', 'mechanic', 'asd@gmail.com', 'images/myprofile.jpg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
